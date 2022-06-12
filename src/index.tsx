8711fc9d50db90e6b0103ac4fd092064763ed71b
saidugurlu <ugurlumsaid@gmx.de>
2022-06-12 17:46:43 +0200

    themeProvider aDDED

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { myTheme } from "./my-theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={myTheme}>
+    <App />
+  </ThemeProvider>
+);
